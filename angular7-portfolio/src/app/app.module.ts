53e72890eb2ca3f6755d5c8037d3ab65615f7b9c
Ricardo Torres <rtorres033@nam.ad.pwcinternal.com>
2019-10-02 18:56:55 -0300

    adding angular7-portfolio

--- angular7-portfolio/src/app/app.module.ts
+++ angular7-portfolio/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { StartComponent } from './start/start.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    StartComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- readme.txt
+++ readme.txt
@@ -4,3 +4,5 @@ https://code.visualstudio.com/docs/nodejs/angular-tutorial
 https://blog.zaven.co/angular-7-tutorial-learn-angular-7-by-example/
 https://stackblitz.com/angular/apvnyeypnyg
 https://www.javatpoint.com/angular-7-with-bootstrap
+https://www.javatpoint.com/angular-7-with-bootstrap
+https://www.techiediaries.com/angular-course/
