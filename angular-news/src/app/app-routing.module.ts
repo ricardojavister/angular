0f9e3bfe9c572489b003930fb49dea5d2ac50a18
Ricardo Torres <rtorres033@nam.ad.pwcinternal.com>
2019-10-24 10:06:59 -0300

    adding routes

--- angular-news/src/app/app-routing.module.ts
+++ angular-news/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeadlinesComponent } from './headlines/headlines.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { SourcesComponent } from './sources/sources.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'home',
+  component: HomeComponent
+},
+{
+  path:'headlines',
+  component: HeadlinesComponent
+},
+{
+  path:'sources',
+  component: SourcesComponent
+},
+{
+  path:'favorites',
+  component: FavoritesComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
